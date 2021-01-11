2898de1f58be6d87a76efecd635cb15a1475a82b
mirdan <syahidmirdan@gmal.com>
2021-01-11 17:56:22 +0700

    Add and change Image result and toast from onCreate fun into rollDice()

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -14,26 +14,34 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
-            val luckyNumber = 4
-            when (rollDice()) {
-                luckyNumber -> Toast.makeText(this, "You are win!", Toast.LENGTH_SHORT).show()
-                1 -> Toast.makeText(this, "You only get 1, not lucky enough!", Toast.LENGTH_SHORT).show()
-                2 -> Toast.makeText(this, "Only get 2 from this roll, try again!", Toast.LENGTH_SHORT).show()
-                3 -> Toast.makeText(this, "You got 3, too small thoug!", Toast.LENGTH_SHORT).show()
-                5 -> Toast.makeText(this, "Good job! but not enough yet!", Toast.LENGTH_SHORT).show()
-                6 -> Toast.makeText(this, "Congrats you are got Extra Point!", Toast.LENGTH_SHORT).show()
-            }
+            rollDice()
         }
 
 
     }
 
-    private fun rollDice(): Int {
+    private fun rollDice() {
         val dice = Dice(6)
-        val result = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = result.toString()
-        return result
+        val diceNum = dice.roll()
+        val luckyNumber = 4
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        when (diceNum) {
+            luckyNumber -> diceImage.setImageResource(R.drawable.dice_4)
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
+
+        when (diceNum) {
+            luckyNumber -> Toast.makeText(this, "You are win!", Toast.LENGTH_SHORT).show()
+            1 -> Toast.makeText(this, "You only get 1, not lucky enough!", Toast.LENGTH_SHORT).show()
+            2 -> Toast.makeText(this, "Only get 2 from this roll, try again!", Toast.LENGTH_SHORT).show()
+            3 -> Toast.makeText(this, "You got 3, too small thoug!", Toast.LENGTH_SHORT).show()
+            5 -> Toast.makeText(this, "Good job! you have got 5, but not enough yet!", Toast.LENGTH_SHORT).show()
+            6 -> Toast.makeText(this, "Congrats you are got Extra Point! cause you get 6!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
 
